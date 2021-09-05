1c19554ed41ae6b93bfab4362421fedd49b2d21d
¨KOnBaTE¨ <karen.bautista@utreee.com>
2021-09-05 12:31:53 -0400

    nuevo karen

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:briefcase/src/pages/dashboard_page.dart';
 import 'package:briefcase/src/pages/history_page.dart';
 import 'package:briefcase/src/pages/login_page.dart';
 import 'package:briefcase/src/pages/transactions/transaction_page.dart';
+import 'package:briefcase/src/pages/product_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -96,6 +97,15 @@ class _HomePageState extends State<HomePage> {
                   context, MaterialPageRoute(builder: (context) => LoginPage()))
             },
           ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.book),
+            title: Text('Productos'),
+            onTap: () => {
+              Navigator.of(context).pop(),
+              Navigator.push(context, MaterialPageRoute(builder: (context) => ProductPage()))
+            },
+          )
         ],
       )),
     );
---
+++ lib/src/pages/product_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProductPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Product'),
+        centerTitle: true,
+        backgroundColor: Colors.white30,
+      ),
+    );
+  }
+}
